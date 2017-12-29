7a6c2c9706382f8a15b61d0b7799499cf617fb75
dogukansonmez <dogukansonmez@gmail.com>
2017-12-29 23:45:49 +0300

    adding print function

--- print_numbers.py
+++ print_numbers.py
@@ -1,13 +1,17 @@
 
+#Prints the number in matrix form
 def print_numbers(number):
-    print(number)
-
+    for i in range(1,number+1):
+        for j in range(1, number+1):
+            print str(i) + " ",
+        print
 
 
 number=int(input("please enter a number"))
+
 if number%2==0:
     print_numbers(number+1)
 else:
     print_numbers(number-1)
-    
+
 
